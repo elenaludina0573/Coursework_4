f10e3bff43d9d65c9026c85c7181ca3ff54de054
elenaludina0573 <elenaludina0@gmail.com>
2024-02-13 10:52:58 +0300

    Information about the vacancy

---
+++ src/vacancy.py
@@ -0,0 +1,19 @@
+class Vacancy:
+    """
+    Информация о вакансии
+    """
+    def __init__(self, vacancy_title, town, salary_from, salary_to, employment, url):
+        self.vacancy_title: str = vacancy_title
+        self.town: str = town
+        self.salary_from: int = salary_from
+        self.salary_to: int = salary_to
+        self.employment: str = employment
+        self.url: str = url
+
+    def __str__(self):
+        return f'название вакансии: {self.vacancy_title}\n' \
+               f'город: {self.town}\n' \
+               f'зарплата от: {self.salary_from}\n' \
+               f'зарплата до: {self.salary_to}\n' \
+               f'тип занятости: {self.employment}\n' \
+               f'ссылка на вакансию: {self.url}\n'
