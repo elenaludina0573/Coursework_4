c4fa82d93995ebbce19c8fde15d3f586e27bc08d
elenaludina0573 <elenaludina0@gmail.com>
2024-02-13 23:10:37 +0300

    List job processing

--- src/vacancy.py
+++ src/vacancy.py
@@ -44,3 +44,30 @@ class Vacancy:
             vacancy_dict['employment'],
             vacancy_dict['url']
         )
+
+
+class Vacancies:
+    """ Обработка списка вакансий"""
+
+    def __init__(self):
+        self.__all_vacancies = []
+
+    def add_vacancies(self, new_vacancies):
+        self.__all_vacancies += new_vacancies
+
+    def delete_vacancies(self, old_vacancies):
+        for i in old_vacancies:
+            self.__all_vacancies.remove(i)
+
+    def sort_vacancies_by_salary(self):
+        self.__all_vacancies.sort(reverse=True)
+
+    @property
+    def all_vacancies(self):
+        return self.__all_vacancies
+
+    def to_list_dict(self):
+        a = []
+        for i in self.__all_vacancies:
+            a.append(i.to_dict())
+        return a
