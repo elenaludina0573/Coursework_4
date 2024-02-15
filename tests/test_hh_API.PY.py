332fb17357c6c2aa6bd51504da24be0019ddfa08
elenaludina0573 <elenaludina0@gmail.com>
2024-02-15 21:00:34 +0300

    Test_hh_API

---
+++ tests/test_hh_API.PY.py
@@ -0,0 +1,21 @@
+import pytest
+from src.vacancy import Vacancy
+from abc import ABC
+from src.abstracted_classes import GetVacancies
+from src.hh_API import HeadHunterAPI
+
+
+@pytest.fixture
+def v():
+    v = Vacancy("Ведущий програмист-разработчик Unity, C#",
+                "Москва",
+                120000,
+                250000,
+                "Полная занятость",
+                "https://hh.ru/vacancy/93159478")
+    return v
+
+
+def test_issubclass():
+    assert issubclass(GetVacancies, ABC)
+    assert issubclass(HeadHunterAPI, GetVacancies)
